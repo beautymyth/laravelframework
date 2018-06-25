b989ea994c90a96e4d75e19bac7f2a0891c9f2d0
beautymyth <37394958+beautymyth@users.noreply.github.com>
2018-06-25 14:55:35 +0800

    Update RegisterFacades.php

--- vendor/laravel/framework/src/Illuminate/Foundation/Bootstrap/RegisterFacades.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Bootstrap/RegisterFacades.php
@@ -16,11 +16,15 @@ class RegisterFacades
      * @return void
      */
     public function bootstrap(Application $app)
-    {
+    {   
+        //清空已解析的实例
         Facade::clearResolvedInstances();
-
+        
+        //设置app实例到facade中
         Facade::setFacadeApplication($app);
-
+        
+        //获取配置的facade+外部包的facade
+        //注册自动加载器
         AliasLoader::getInstance(array_merge(
             $app->make('config')->get('app.aliases', []),
             $app->make(PackageManifest::class)->aliases()
